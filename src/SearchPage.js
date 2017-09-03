670548b7d500e35c3452166447c4cba8e073aefe
ChrisKrawietz <chriskrawietz@gmail.com>
2017-09-03 15:01:24 +0200

    Bugfixed Searchterm. currentlyReading Property should work now

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import Book from './Book'
 import * as BooksAPI from './BooksAPI'
@@ -16,54 +16,57 @@ class SearchPage extends Component {
     wantToRead: [],
     read: []
   }
+
   componentDidMount() {
     this.inputSearch.focus();
   }
-  onChangeSearchQuery= event => {
-    const query = event .target.value.trim()
-    const allExistingBooks = this.props.exinstingBooks['currentlyReading'].shelfBooks
+
+  onChangeSearchQuery = event => {
+    const query = event.target.value.trim()
+    const allExistingBooks = this.props.existingBooks['currentlyReading'].shelfBooks
       .concat(this.props.existingBooks['wantToRead'].shelfBooks)
       .concat(this.props.existingBooks['read'].shelfBooks)
 
-    if(query) {
+    if (query) {
       BooksAPI.search(query, 20)
-      .then(response => {
-        if(response && response.length > 0) {
-          this.setState({
-            resultingBooks: response.map(item => {
-              const bookIndex = allExistingBooks.findIndex(bookItem => bookItem.id === item.id)
-              if(bookIndex >= 0) {
-                return Object.assign({}, item, { shelf: allExistingBooks[bookIndex].shelf})
-              }
-              else {
-                return item
-              }
+        .then(response => {
+          if (response && response.length > 0) {
+            this.setState({
+              resultingBooks: response.map(item => {
+                const bookIndex = allExistingBooks.findIndex(bookItem => bookItem.id === item.id)
+                if (bookIndex >= 0) {
+                  return Object.assign({}, item, { shelf: allExistingBooks[bookIndex].shelf })
+                } else {
+                  return item
+                }
+              })
             })
-          })
-        } else {
-          this.setState({ resultingBooks: [] })
-        }
-      })
+          } else {
+            this.setState({ resultingBooks: [] })
+          }
+        })
     } else {
       this.setState({ resultingBooks: [] })
     }
   }
-  findBookinShelf = book => {
+
+  findBookInShelf = book => {
     let bookFounded = {
       id: -1,
       shelf: ''
     }
     const allSelectedBooks = this.selectedBooks['currentlyReading']
-      .concat(this.existingBooks['wantToRead'])
-      .concat(this.existingBooks['read'])
+      .concat(this.selectedBooks['wantToRead'])
+      .concat(this.selectedBooks['read'])
     const bookIndex = allSelectedBooks.findIndex(item => item.id === book.bookReference.id)
     bookFounded.id = bookIndex
-    if(bookIndex >= 0) {
+    if (bookIndex >= 0) {
       bookFounded.shelf = allSelectedBooks[bookIndex].shelf
     }
 
     return bookFounded
   }
+
   onChangeShelf = (targetShelfName, book) => {
     let bookFounded = this.findBookInShelf(book)
     if (targetShelfName !== 'none') {
@@ -89,20 +92,24 @@ class SearchPage extends Component {
       }
     }
   }
+
   onChangeRoute = () => {
-    this.props.onAddBookShelfs(this.selectedBooks)
+    this.props.onAddBooksShelfs(this.selectedBooks)
   }
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <a className="close-search" style={{ cursor: 'pointer' }} onClick={this.onChangeRoute}>
+            Back to Dashboard
+          </a>
           <div className="search-books-input-wrapper">
             <input type="text"
               placeholder="Search by title or author"
-              value="{this.state.value}"
+              value={this.state.value}
               onKeyUp={event => this.onChangeSearchQuery(event)}
-              ref={(input) => {this.inputSearch = input;}} />
+              ref={(input) => { this.inputSearch = input; }} />
           </div>
         </div>
         <div className="search-books-results">
