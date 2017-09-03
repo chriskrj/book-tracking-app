3cb02d28c428c8fc6ce52afa0d3caaab38a5d0b4
ChrisKrawietz <chriskrawietz@gmail.com>
2017-09-03 13:14:24 +0200

    created BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(
+  // Router for Searchpage
+  <BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root'));
+
+)
+registerServiceWorker();
